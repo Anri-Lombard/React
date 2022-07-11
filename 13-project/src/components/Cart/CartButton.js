c02d391cb08fa28dadc03941f6857bdb6e499b8b
Anri Lombard <anri.m.lombard@gmail.com>
2022-07-11 21:25:46 +0200

    Project 13

--- 13-project/src/components/Cart/CartButton.js
+++ 13-project/src/components/Cart/CartButton.js
@@ -1,18 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
-import {useDispatch} from 'react-redux';
-import uiActions from '../../store/ui-slice';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
-    dispatch(uiActions);
+    dispatch(uiActions.toggle());
   };
 
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- 13-project/src/index.js
+++ 13-project/src/index.js
@@ -1,8 +1,13 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import store from './store/index';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
